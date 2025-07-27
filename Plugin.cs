3d4a132a6f1a2ae4f5b9e66c2ef89a6ef6c21191
Kupie <kupokraft@gmail.com>
2025-07-27 14:05:53 -0500

    Update for 1.0

--- Plugin.cs
+++ Plugin.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 using HarmonyLib;
 //StartScreen to modify NDA popup
 using Assets.Scripts.Components.StartScreen;
+using Systems.PersistentProgress;
+using System;
+using UI.Components.StartScreen;
 
-namespace MainNameSpace
+namespace WanderingQoL
 {
     [BepInPlugin(PluginGuid, PluginName, PluginVer)]
     [BepInProcess(PluginTargetExe)]
@@ -16,7 +19,7 @@ namespace MainNameSpace
     {
         public const string PluginGuid = "org.Kupie.WanderingQoL";
         public const string PluginName = "WanderingQoL";
-        public const string PluginVer = "1.0.0";
+        public const string PluginVer = "1.0.4";
         public const string PluginTargetExe = "WanderingVillage.exe";
         internal static ManualLogSource Log;
         
@@ -24,13 +27,17 @@ namespace MainNameSpace
         // Config Bindings
         internal static ConfigEntry<bool> SkipWelcome;
         internal static ConfigEntry<bool> RemoveSocials;
+        internal static ConfigEntry<bool> SkipSurvey;
         internal static ConfigEntry<bool> MuteOnUnfocus;
         internal static ConfigEntry<KeyCode> MinusWorker;
         internal static ConfigEntry<KeyCode> PlusWorker;
         internal static ConfigEntry<KeyCode> MuteHotkey;
+        internal static ConfigEntry<int> MaxSpeed;
         internal static ConfigEntry<bool> ExtraWorkerLimits;
+        internal static ConfigEntry<float> WorkerSpeedModVar;
+        internal static ConfigEntry<bool> UnlockHostilityModifiers;
         internal static ConfigEntry<bool> DebugLogging;
-
+        
 
         private void Awake()
         {
@@ -43,6 +50,7 @@ namespace MainNameSpace
             //General
             SkipWelcome = Config.Bind("1. General", "Remove Welcome", true, "Remove Welcome Screen");
             RemoveSocials = Config.Bind("1. General", "Remove Socials", true, "Removes Social Media links");
+            SkipSurvey = Config.Bind("1. General", "Skip Survey on Quit", true, "Skips the 'take a survey' screen upon quitting");
             MuteOnUnfocus = Config.Bind("1. General", "Mute On Unfocus", true, "Mute when Game Loses Focus");
 
             MuteHotkey = Config.Bind("1. General", "Game Mute Hotkey with CTRL", KeyCode.M, "Holding CTRL + This key will mute/unmute the game");
@@ -50,9 +58,15 @@ namespace MainNameSpace
             PlusWorker = Config.Bind("1. General", "Hotkey to add a worker", KeyCode.X, "Hotkey to Add a Worker");
             MinusWorker = Config.Bind("1. General", "Hotkey to subtract a worker", KeyCode.Z, "Hotkey to Subtract a Worker");
 
+
+            MaxSpeed = Config.Bind("1. General", "Max Speed", 4, "Maximum Speed the game will let you run it at");
+
+            UnlockHostilityModifiers = Config.Bind("1. General", "Force Unlock Hostility modifiers", false, "Unlocks hostility modifiers even if you haven't beat the game");
+
             //Cheats
             ExtraWorkerLimits = Config.Bind("2. Cheats", "Extra Workers", false, "Allow up to 13 workers per building");
-
+            WorkerSpeedModVar = Config.Bind("2. Cheats", "Worker Move Speed Multiplier", 1.0f, "Modifier worker move speed by this amount. '2' would make them move 2x as fast.");
+            
             //Debug
             DebugLogging = Config.Bind("3. Debug", "Debug Logging", false, "Enables Debug Logging");
 
@@ -199,6 +213,13 @@ namespace MainNameSpace
 
                     }
                 }
+                if (SkipSurvey.Value && Game.Settings.ActiveDemoSettings.ShowSurveyOnLeave)
+                {
+                    Plugin.D.L("Setting ShowSurveyOnLeave to false", "info");
+                    Game.Settings.ActiveDemoSettings.ShowSurveyOnLeave = false;
+                }
+
+
             }
         }
         //Hiding Socials off Pause Menu
@@ -217,9 +238,70 @@ namespace MainNameSpace
             }
         }
 
+        ////Unlock Hostility Modifiers
+        //[HarmonyPatch(typeof(DifficultyModifiersPanel), nameof(DifficultyModifiersPanel.AbsoluteMaxDifficulty), MethodType.Getter)]
+        //class UnlockHostilityModifiersClass
+        //{
+        //    static void Postfix(ref int __result)
+        //    {
+        //        D.L("Current Max Difficulty: " + __result, "info");
+        //        __result = 100;
+        //    }
+        //}
+
+        //[HarmonyPatch(typeof(PersistentProgressSaveDataExtensions), nameof(PersistentProgressSaveDataExtensions.ShouldAutoUnlockDifficultyModifiers))]
+        //class UnlockHostilityModifiersClass2
+        //{
+        //
+        //    static void Prefix()
+        //    {
+        //        if (Plugin.UnlockHostilityModifiers.Value && !PersistentProgressSaveSystem.SaveData.AreDifficultyModifiersUnlocked)
+        //        {
+        //            D.L("Patching ShouldAutoUnlockDifficultyModifiers", "info");
+        //            PersistentProgressSaveSystem.SaveData.AreDifficultyModifiersUnlocked = true;
+        //        }
+        //    }
+        //}
+
+        // Game speed mod
+        [HarmonyPatch(typeof(TimeSystem), nameof(TimeSystem.GetMaxSpeed))]
+        class TimeSystemMod
+        {
+            static void Postfix(ref int __result)
+            {
+                if (!Game.Data.DebugData.UnlockTimeScale)
+                {
+                    __result = MaxSpeed.Value;
+                }
+            }
+        }
+
+        // Worker Movespeed modifier
+        [HarmonyPatch(typeof(Worker), nameof(Worker.GetMovementSpeed))]
+        class WorkerSpeedMod
+        {
+            static void Postfix(ref float __result)
+            {
+                __result *= WorkerSpeedModVar.Value;
+            }
+        }
+
+        [HarmonyPatch(typeof(Game), nameof(Game.Update))]
+        class AutoAcceptNomads
+        {
+            static void Postfix()
+            {
+                try
+                {
+                    UpkeepSystem.TryAcceptReproductionSpawnedWorkers(Game.Data.World);
+                }
+                catch (Exception e) { }
+            }
+        }
 
         // Trying to implement "cheats" portion into GUI...
         //[HarmonyPatch(typeof(WorkerSection), nameof(WorkerSection.Init))]
+
         //class WorkerOverride
         //{
         //    
@@ -249,30 +331,54 @@ namespace MainNameSpace
         //}
 
         // PlusMinus hotkeys stuff
+        [HarmonyPatch(typeof(TitleSectionOccupant), nameof(TitleSectionOccupant.UpdateUi))]
+        class DrawWorkerCount
+        {
+            static void Postfix(ref FieldOccupant occupant, TitleSectionOccupant __instance)
+            {
+                if (Plugin.ExtraWorkerLimits.Value && occupant.TargetWorkerCount > 1) {
+                    string stringToAdd = " (" + occupant.WorkerCount + "/" + occupant.TargetWorkerCount.ToString() + ")";
+                    if (occupant.HasFunction(BuildingFunction.RadioTower) && occupant.State == FieldOccupantState.Complete)
+                    {
+                        __instance.Name.SetLocalizedString(occupant.Settings.Building.RadioTower.CompletedNameOverride.ToString() + stringToAdd);
+                    }
+                    else
+                    {
+                        __instance.Name.SetLocalizedString(occupant.Settings.Name.ToString() + stringToAdd);
+                    }
+            
+                }
+            }
+        }
         [HarmonyPatch(typeof(WorkerSection), nameof(WorkerSection.UpdateUi))]
         class PlusMinusHotkeys
         {
-            static void Postfix(World world,  FieldOccupant occupant, WorkerSection __instance)
+            static void Postfix(ref FieldOccupant occupant, WorkerSection __instance)
             {
-                bool flag = occupant.FullStaffRequired();
+                //bool flag = occupant.FullStaffRequired();
+                bool flag = __instance._fullStaff;
                 int num = flag ? __instance._maxWorkerCount : 1;
                 int maxWorkers = __instance._maxWorkerCount;
+                bool BlacklistedExtraWorkerBuilding = occupant.HasFunction(BuildingFunction.Research) |
+                    occupant.HasFunction(BuildingFunction.Research
+                    );
                 // Set limit to 13 if using the cheat for it
-                if (Plugin.ExtraWorkerLimits.Value)
+                if (Plugin.ExtraWorkerLimits.Value && maxWorkers > 1 && !BlacklistedExtraWorkerBuilding)
                 {
                     maxWorkers = 13;
+
                 }
                 if (Input.GetKeyUp(Plugin.PlusWorker.Value))
                 {
                     occupant.TargetWorkerCount = (byte)Mathf.Min((int)occupant.TargetWorkerCount + num, maxWorkers);
                     D.L("Updated Max Workers: " + occupant.TargetWorkerCount.ToString(), "info");
-                    __instance.UpdateButtonVisuals(occupant, flag, false);
+                    __instance.UpdateButtonVisuals(occupant, occupant.TargetWorkerCount, false, true, occupant.IsOnMission());
                 }
                 if (Input.GetKeyUp(Plugin.MinusWorker.Value))
                 {
                     occupant.TargetWorkerCount = (byte)Mathf.Max((int)occupant.TargetWorkerCount - num, 0);
                     D.L("Updated Max Workers: " + occupant.TargetWorkerCount.ToString(), "info");
-                    __instance.UpdateButtonVisuals(occupant, flag, false);
+                    __instance.UpdateButtonVisuals(occupant, occupant.TargetWorkerCount, false, true, occupant.IsOnMission());
                 }
 
             }
